67308874b9255cdfc8e4868a0d648649b3c4ed41
fareed23 <faroodcp9@gmail.com>
2023-05-29 21:14:47 +0500

    Sets

--- hash.py
+++ hash.py
@@ -1,4 +1,13 @@
 # Hash is used to compare the dictionary keys
+
+# n = int(input())
+# t = tuple(map(int,input().split()))
+# # tu = tuple(t)
+# print(hash(t))
+
 n = int(input())
 t = tuple(map(int, input().split()))
-print(hash(t))
+string_representation = ' '.join(map(str, t))
+hash_value = hash(string_representation)
+print(hash_value)
+
---
+++ sets_average.py
@@ -0,0 +1,14 @@
+def average(array):
+    # your code goes here
+    # return sum(set(array)) / len(set(n))
+    my_set = set(array)
+    avg = sum(my_set) / len(my_set)
+    return avg
+    
+
+if __name__ == '__main__':
+        n = int(input())
+        arr = list(map(int, input().split()))
+        result = average(arr)
+        print(result)
+
