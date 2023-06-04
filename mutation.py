2abed5b2233d598ae6af2c4195fced667564a6dc
fareed23 <faroodcp9@gmail.com>
2023-06-04 18:46:47 +0500

    Find a string

--- mutation.py
+++ mutation.py
@@ -1,11 +1,14 @@
-# def mutate_string(string, position, character):
-#     lst = list(string) # convert string into a list 
-#     lst[position] = character # assign character at the specified position
-#     name = ''.join(lst) # join the list back into a string
-#     return name
+# Method 1 through converting into a list and then change
+
+def mutate_string(string, position, character):
+    lst = list(string) # convert string into a list 
+    lst[position] = character # assign character at the specified position
+    name = ''.join(lst) # join the list back into a string
+    return name
 
 
 # Method 2 through slicing
+
 def mutate_string(string, position, character):
     lst = list(string)
     name = string[:position] + character + string[position:]
