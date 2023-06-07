d008ab2d6b735ca857cb518c07513a6187101bba
fareed23 <faroodcp9@gmail.com>
2023-06-07 17:23:57 +0500

    Text wrap

---
+++ text_wrap.py
@@ -0,0 +1,30 @@
+import textwrap
+
+def wrap(string, max_width):
+    # Method 1
+    # wrapresult = textwrap.fill(string, max_width)
+    # return wrapresult
+
+    # Method 2
+    convert = list(string)
+
+    lines = []
+    line = ""
+
+    for i in convert:
+        if len(line) < max_width:
+            line += i
+
+        else:
+            lines.append(line)
+            line = i
+
+    lines.append(line)
+
+    return "\n".join(lines)
+
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
